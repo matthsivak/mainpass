99d3206bd67659de298ae184de24ff0f95e60cf1
Matthew Sivák <matthew@thesivak.net>
2022-06-01 16:43:38 +0200

    WIP
    Co-authored-by: sebosh <sebosh@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 //  print!();
 //  stdout.flush().unwrap();
 
+const LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
+const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const DIGITS: &str = "0123456789";
+
 use crossterm::{
   cursor,
   event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
@@ -111,7 +115,7 @@ fn login_register_menu() {
   }
 }
 
-fn create_file(username: String, password: String) -> bool {
+fn create_file(username: String, password: String) -> Result<Option<String>, String> {
   let mc = new_magic_crypt!(&password, 256);
 
   let base64 = mc.encrypt_str_to_base64(format!(
@@ -127,11 +131,101 @@ welcome = welcome
 
   let filename = format!("data_{}.simpass", username);
 
-  match fs::write(&filename, base64) {
-    Err(..) => false,
-    Ok(..) => true,
+  let file_info = fs::metadata(&filename).is_ok();
+
+  if file_info {
+    Err("User already exists".to_string())
+  } else {
+    match fs::write(&filename, base64) {
+      Err(..) => Err("Cant save data".to_string()),
+      Ok(..) => Ok(None),
+    }
   }
 }
+
+fn register_username_loop() -> String {
+  let mut stdout = stdout();
+
+  loop {
+    print!("\rUsername: ");
+    stdout.flush().unwrap();
+
+    let mut username = String::new();
+
+    enable_raw_mode().unwrap();
+
+    loop {
+      match read().unwrap() {
+        Event::Key(KeyEvent {
+          code: KeyCode::Char(c),
+          modifiers: KeyModifiers::NONE,
+        }) => {
+          username.push(c);
+          print!("{}", c);
+          stdout.flush().unwrap();
+        }
+        Event::Key(KeyEvent {
+          code: KeyCode::Char(c),
+          modifiers: KeyModifiers::SHIFT,
+        }) => {
+          username.push(c.to_ascii_uppercase());
+          print!("{}", c.to_ascii_uppercase());
+          stdout.flush().unwrap();
+        }
+        Event::Key(KeyEvent {
+          code: KeyCode::Backspace,
+          modifiers: KeyModifiers::NONE,
+        }) => {
+          if username.len() > 0 {
+            username.pop();
+            print!("\x08 \x08");
+            stdout.flush().unwrap();
+          }
+        }
+        Event::Key(KeyEvent {
+          code: KeyCode::Enter,
+          modifiers: KeyModifiers::NONE,
+        }) => break,
+        _ => (),
+      }
+    }
+
+    if username.len() == 0 {
+      println!("\r\nUsername is empty");
+      continue;
+    }
+
+    let file_info = fs::metadata(&format!("data_{}.simpass", username)).is_ok();
+
+    if file_info {
+      println!("\r\nUser already exists");
+      continue;
+    }
+
+    return username.to_string();
+  }
+}
+
+fn load_file(username: String, password: String) -> Result<String, String> {
+  let mc = new_magic_crypt!(&password, 256);
+  let filename = format!("data_{}.simpass", &username);
+
+  let file_info = fs::metadata(&filename).is_ok();
+
+  if !file_info {
+    Err("File not found".to_string())
+  } else {
+    match fs::read_to_string(filename) {
+      Err(..) => Err("Something went wrong while reading data from disk".to_string()),
+      Ok(c) => match mc.decrypt_base64_to_string(c) {
+        Ok(d) => Ok(d),
+        Err(..) => Err("Wrong password".to_string()),
+      },
+    }
+  }
+}
+
+// Register
 
 fn register() {
   clear();
@@ -140,40 +234,8 @@ fn register() {
   let mut stdout = stdout();
 
   println!("\r********** REGISTER **********\n");
-  print!("Username: ");
-  stdout.flush().unwrap();
 
-  let mut username = String::new();
-
-  enable_raw_mode().unwrap();
-
-  loop {
-    match read().unwrap() {
-      Event::Key(KeyEvent {
-        code: KeyCode::Char(c),
-        modifiers: KeyModifiers::NONE,
-      }) => {
-        username.push(c);
-        print!("{}", c);
-        stdout.flush().unwrap();
-      }
-      Event::Key(KeyEvent {
-        code: KeyCode::Backspace,
-        modifiers: KeyModifiers::NONE,
-      }) => {
-        if username.len() > 0 {
-          username.pop();
-          print!("\x08 \x08");
-          stdout.flush().unwrap();
-        }
-      }
-      Event::Key(KeyEvent {
-        code: KeyCode::Enter,
-        modifiers: KeyModifiers::NONE,
-      }) => break,
-      _ => (),
-    }
-  }
+  let username = register_username_loop();
 
   loop {
     print!("\n\rPassword: ");
@@ -189,7 +251,15 @@ fn register() {
           modifiers: KeyModifiers::NONE,
         }) => {
           password.push(c);
-          print!("*");
+          print!("{}", c);
+          stdout.flush().unwrap();
+        }
+        Event::Key(KeyEvent {
+          code: KeyCode::Char(c),
+          modifiers: KeyModifiers::SHIFT,
+        }) => {
+          password.push(c.to_ascii_uppercase());
+          print!("{}", c.to_ascii_uppercase());
           stdout.flush().unwrap();
         }
         Event::Key(KeyEvent {
@@ -220,7 +290,15 @@ fn register() {
           modifiers: KeyModifiers::NONE,
         }) => {
           password_confirmation.push(c);
-          print!("*");
+          print!("{}", c);
+          stdout.flush().unwrap();
+        }
+        Event::Key(KeyEvent {
+          code: KeyCode::Char(c),
+          modifiers: KeyModifiers::SHIFT,
+        }) => {
+          password_confirmation.push(c.to_ascii_uppercase());
+          print!("{}", c.to_ascii_uppercase());
           stdout.flush().unwrap();
         }
         Event::Key(KeyEvent {
@@ -241,36 +319,57 @@ fn register() {
       }
     }
 
+    // Min 1 uppercase, min 1 lowercase, min 1 digit, min 8 chars
+
+    let mut uppercase = false;
+    let mut lowercase = false;
+    let mut number = false;
+
+    for c in password.chars() {
+      if UPPERCASE.contains(c) {
+        uppercase = true;
+      }
+      if LOWERCASE.contains(c) {
+        lowercase = true;
+      }
+      if DIGITS.contains(c) {
+        number = true;
+      }
+    }
+    if password.len() < 8 {
+      println!("\r\nPassword must be at least 8 characters");
+      continue;
+    }
+    if !uppercase {
+      println!("\r\nPassword must contain at least 1 uppercase letter");
+      continue;
+    }
+    if !lowercase {
+      println!("\r\nPassword must contain at least 1 lowercase letter");
+      continue;
+    }
+    if !number {
+      println!("\r\nPassword must contain at least 1 digit");
+      continue;
+    }
+
     if password == password_confirmation {
       clear();
-      println!("Success");
+      create_file(username, password).unwrap();
       break;
     } else {
       clear();
-      println!("Passwords didnt match");
+      println!(
+        "Passwords didnt match\r\n{} {}",
+        password, password_confirmation
+      );
+      continue;
     }
   }
 
   if was {
-    enable_raw_mode();
+    enable_raw_mode().unwrap();
   }
 }
 
-fn load_file(username: String, password: String) -> Result<String, String> {
-  let mc = new_magic_crypt!(&password, 256);
-  let filename = format!("data_{}.simpass", &username);
-
-  let file_info = fs::metadata(&filename).is_ok();
-
-  if !file_info {
-    Err("File not found".to_string())
-  } else {
-    match fs::read_to_string(filename) {
-      Err(..) => Err("Something went wrong while reading data from disk".to_string()),
-      Ok(c) => match mc.decrypt_base64_to_string(c) {
-        Ok(d) => Ok(d),
-        Err(..) => Err("Wrong password".to_string()),
-      },
-    }
-  }
-}
+// Register end
